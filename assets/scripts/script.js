6d5fc5a2ec763919de563620c4d514487db752cc
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2021-01-29 02:03:49 -0300

    Displaying the modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,12 +5,16 @@ pizzaJson.map((item, key) => {
   let pizzaItem = selec(".models .pizza-item").cloneNode(true);
 
   pizzaItem.querySelector(".pizza-item--img img").src = item.img;
-
   pizzaItem.querySelector(
     ".pizza-item--price"
   ).innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+    selec(".pizzaWindowArea").style.display = "flex";
+  });
 
   selec(".pizza-area").append(pizzaItem);
 });
