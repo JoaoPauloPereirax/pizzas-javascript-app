37a0010d5ef26a1b578b9e25284ab1437995b217
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2021-01-29 01:38:20 -0300

    Showing information on the page

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+const selec = (el) => document.querySelector(el);
+const selecAll = (el) => document.querySelectorAll(el);
+
+pizzaJson.map((item, key) => {
+  let pizzaItem = selec(".models .pizza-item").cloneNode(true);
+
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+
+  selec(".pizza-area").append(pizzaItem);
+});
