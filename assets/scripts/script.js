fb45e58ed0b670f3eb694895820566d9cdc42362
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2021-02-04 10:38:22 -0300

    Adding information to the cart

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -109,8 +109,15 @@ function updateCart() {
   if (cart.length > 0) {
     selec("aside").classList.add("show");
     selec(".cart").innerHTML = "";
+
+    let subtotal = 0;
+    let desconto = 0;
+    let total = 0;
+
     for (let i in cart) {
       let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+
+      subtotal += pizzaItem.price * cart[i].qt;
 
       let cartItem = selec(".models .cart--item").cloneNode(true);
 
@@ -131,9 +138,32 @@ function updateCart() {
       cartItem.querySelector("img").src = pizzaItem.img;
       cartItem.querySelector(".cart--item-nome").innerHTML = pizzaName;
       cartItem.querySelector(".cart--item--qt").innerHTML = cart[i].qt;
+      cartItem
+        .querySelector(".cart--item-qtmenos")
+        .addEventListener("click", () => {
+          if (cart[i].qt > 1) {
+            cart[i].qt--;
+          } else {
+            cart.splice(i, 1);
+          }
+          updateCart();
+        });
+      cartItem
+        .querySelector(".cart--item-qtmais")
+        .addEventListener("click", () => {
+          cart[i].qt++;
+          updateCart();
+        });
 
       selec(".cart").append(cartItem);
     }
+
+    desconto = subtotal * 0.1;
+    total = subtotal - desconto;
+
+    selec(".subtotal span:last-child").innerHTML = `R$ ${subtotal.toFixed(2)}`;
+    selec(".desconto span:last-child").innerHTML = `R$ ${desconto.toFixed(2)}`;
+    selec(".total span:last-child").innerHTML = `R$ ${total.toFixed(2)}`;
   } else {
     selec("aside").classList.remove("show");
   }
